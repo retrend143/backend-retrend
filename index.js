cd5a489831ef16d52768f47d31a9cbf2462d7956
retrend143 <retrend159@gmail.com>
2025-06-05 15:42:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ dotenv.config();
 const app = express();
 
 // ✅ Proper CORS Configuration
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "https://retrendonline-in.vercel.app", credentials: true }));
 
 app.use(bodyParser.json());
 
